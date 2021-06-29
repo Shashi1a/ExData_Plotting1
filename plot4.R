61832909e56ca29f807ddf0228357f5ae050c98a
shashikant <skunwar1a@gmail.com>
2021-06-29 17:15:00 +0530

    code to load data and plot multiple plots in one output file

--- plot4.R
+++ plot4.R
@@ -1,5 +1,29 @@
+###  this function loads the data and then plots various quantities in a single plot
+### the total number of plots is decided by mfrow row parameter in par.
+
+
 plot4<-function(data=data){
+    # filename that has the data
+    filename="household_power_consumption.txt"
     
+    # loading the file into dataframe d1
+    d1<-read.delim2(filename,header = TRUE,sep=";")
+    
+    # obtaining data into date format
+    d1$Date<-as.Date(d1$Date,format='%d/%m/%Y')
+    
+    #selecting data for the given duration        
+    data<-d1[d1$Date>="2007-02-01" & d1$Date<="2007-02-02" ,]
+    
+    # adding a column of weekdays into the data
+    data<-cbind(data,weekday=weekdays(data$Date))
+    tmax<-dim(data)[1]-1
+    
+    # adding a column that corresponds to mins into the data
+    data<-cbind(data,mins=c(0:tmax))
+    
+    
+    # output filename
     png(filename="plot4.png",width = 480,height = 480)
     par(mfrow=c(2,2))
     
