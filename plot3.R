404b84f77017a74d84a3606310ab859ceb10f973
shashikant <skunwar1a@gmail.com>
2021-06-29 17:12:30 +0530

    added and successfully compiled code to load and generate the plot for plot3

--- plot3.R
+++ plot3.R
@@ -3,8 +3,29 @@
 ### Different submetering are represented with different color shown in the legends
 
 
-plot3<-function(data=data){
+plot3<-function(){
+    
+    # filename that has the data
+    filename="household_power_consumption.txt"
+    
+    # loading the file into dataframe d1
+    d1<-read.delim2(filename,header = TRUE,sep=";")
+    
+    # obtaining data into date format
+    d1$Date<-as.Date(d1$Date,format='%d/%m/%Y')
+    
+    #selecting data for the given duration        
+    data<-d1[d1$Date>="2007-02-01" & d1$Date<="2007-02-02" ,]
+    
+    # adding a column of weekdays into the data
+    data<-cbind(data,weekday=weekdays(data$Date))
+    tmax<-dim(data)[1]-1
+    
+    # adding a column that corresponds to mins into the data
+    data<-cbind(data,mins=c(0:tmax))
+    
     # to save the plot
+    
     png(filename = 'plot3.png',height = 480,width=480)
         
     # plot the data using dataframe
